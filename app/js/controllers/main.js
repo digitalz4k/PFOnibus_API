8ce1adb63945487a9076c82a01d826e762a7e087
digitalz4k <digitalz4k@gmail.com>
2014-06-15 17:58:41 -0300

    Add js directory.

---
+++ app/js/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sdkApp')
+    .controller('MainCtrl', function ($scope, User) {
+        $scope.brand = "Digitalz";
+        $scope.userId = User.id;
+        $scope.name = User.name;
+        $scope.role = User.role;
+        $scope.email = User.email;
+    });
